67309fdb24217d674bf303dc9abbb317983ad381
ImElio <peruginoantonio573@gmail.com>
2025-08-30 15:15:06 +0200

    removing comments [ignore]

--- src/modules/player/PlayerMod.cpp
+++ src/modules/player/PlayerMod.cpp
@@ -1,21 +1,18 @@
 #include "PlayerMod.hpp"
 #include "script.h"
 
-// Stati
 bool PlayerMod::godMode = false;
 bool PlayerMod::neverWanted = false;
 bool PlayerMod::noRagdoll = false;
 bool PlayerMod::superJump = false;
 bool PlayerMod::fastRun = false;
 
-// Toggle
 void PlayerMod::ToggleGodMode() { godMode = !godMode; }
 void PlayerMod::ToggleNeverWanted() { neverWanted = !neverWanted; }
 void PlayerMod::ToggleNoRagdoll() { noRagdoll = !noRagdoll; }
 void PlayerMod::ToggleSuperJump() { superJump = !superJump; }
 void PlayerMod::ToggleFastRun() { fastRun = !fastRun; }
 
-// Actions
 void PlayerMod::HealFull() {
     Ped ped = PLAYER::PLAYER_PED_ID();
     int maxHealth = ENTITY::GET_ENTITY_MAX_HEALTH(ped);
@@ -28,7 +25,6 @@ void PlayerMod::ClearWanted() {
     PLAYER::SET_PLAYER_WANTED_LEVEL_NOW(pid, false);
 }
 
-// Tick
 void PlayerMod::Tick() {
     Ped ped = PLAYER::PLAYER_PED_ID();
     Player pid = PLAYER::PLAYER_ID();
@@ -57,21 +53,18 @@ void PlayerMod::Tick() {
     else         PLAYER::SET_RUN_SPRINT_MULTIPLIER_FOR_PLAYER(pid, 1.0f);
 }
 
-// Pointers per Toggle
 bool* PlayerMod::GodModePtr() { return &godMode; }
 bool* PlayerMod::NeverWantedPtr() { return &neverWanted; }
 bool* PlayerMod::NoRagdollPtr() { return &noRagdoll; }
 bool* PlayerMod::SuperJumpPtr() { return &superJump; }
 bool* PlayerMod::FastRunPtr() { return &fastRun; }
 
-// Getters
 bool PlayerMod::IsGodMode() { return godMode; }
 bool PlayerMod::IsNeverWanted() { return neverWanted; }
 bool PlayerMod::IsNoRagdoll() { return noRagdoll; }
 bool PlayerMod::IsSuperJump() { return superJump; }
 bool PlayerMod::IsFastRun() { return fastRun; }
 
-// Setters
 void PlayerMod::SetGodMode(bool v) { godMode = v; }
 void PlayerMod::SetNeverWanted(bool v) { neverWanted = v; }
 void PlayerMod::SetNoRagdoll(bool v) { noRagdoll = v; }
